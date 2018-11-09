c093d61496f67e2c122e8174e89e3ac5444026ba
Chris <ju66ernautdev@gmail.com>
2018-11-09 10:26:42 -0800

    subject?
    removing empty data prop

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import Vue from "vue";
+import App from "./App";
 
-import store from './store'
-Vue.config.productionTip = false
+import store from "./store";
+Vue.config.productionTip = false;
 Vue.prototype.$eventHub = new Vue();
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   store,
-  template: '<App/>',
+  template: "<App/>",
   components: { App }
-})
+});
